2c649d1dd06a85fdabca6d8fa25f16107e9b4d69
yo <turtleegg@gmail.com>
2017-06-10 23:57:46 +0800

    update pick.py

--- pick/pick.py
+++ pick/pick.py
@@ -36,9 +36,9 @@ def test_one(stock_code, date):
     line_number = retriever.search_line_number_by_date(stock_code, date)
     check = retriever.check_condition_1(stock_code, line_number)
 
-def simulate(stock_codes, date):
+def simulate_and_get_roi(stock_codes, date):
     # TODO: set max_days
-    max_days = 3000
+    max_days = 90
     retriever = Retriever()
     total_cost = 0
     total_revenue = 0
@@ -71,27 +71,42 @@ def simulate(stock_codes, date):
     print('模擬開始日期 {}, 結束日期 {}'.format(date, sell_date))
     #print(summary)
     print('總支出: {}, 總營收: {}, 投報率: {}'.format(formatted_cost, formatted_revenue, formatted_roi))
+    return total_roi
 
 def main():
-    start_date_tw = '106/04/02'
+    start_date_tw = '104/03/02'
     start_date = '{}/{}/{}'.format(int(start_date_tw.split('/')[0]) + 1911, start_date_tw.split('/')[1], start_date_tw.split('/')[2])
     start_date_obj = datetime.datetime.strptime(start_date, "%Y/%m/%d")
-    day_count = 30
+    day_count = 20
     i = 0
     counter = 0
+    wins = []
+    fairs = []
+    loses = []
     while counter < day_count:
         date_obj = start_date_obj + datetime.timedelta(i)
         i += 1
         date = date_obj.strftime("%Y/%m/%d")
         date_tw = '{}/{}/{}'.format(int(date.split('/')[0]) - 1911, date.split('/')[1], date.split('/')[2])
         winners = pick(date_tw)
-        #winners = [1707, 1802, 2008, 2230, 2477, 3048, 3051, 3141, 3434, 3455, 3516, 3556, 4543, 5383, 6105, 6569, 8446]
         try:
-            simulate(winners, date_tw)
+            roi = simulate_and_get_roi(winners, date_tw)
+            if roi > 0:
+                wins.append(roi)
+            else:
+                loses.append(roi)
             counter += 1
         except Exception as e:
             #print('skip {}'.format(date_tw))
             continue
+    wins.sort(reverse=True)
+    loses.sort()
+    wins_count = len(wins)
+    loses_count = len(loses)
+    win_rate = round(float(wins_count) / float(wins_count + loses_count), 4)
+    print('勝 {}: {}'.format(wins_count, ', '.join('{}%'.format(round(roi * 100, 2)) for roi in wins)))
+    print('負 {}: {}'.format(loses_count, ', '.join('{}%'.format(round(roi * 100, 2)) for roi in loses)))
+    print('勝率: {}%'.format(win_rate * 100, ".2f"))
 
 if __name__ == '__main__': main()
 
